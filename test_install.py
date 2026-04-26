423f90124b0a2a36e1219511e294f51e805a17af
NEMALA JAYARAM SATYA SANTOSH <24a31a0247@pragati.ac.in>
2026-04-26 23:23:36 +0530

    Add test install message for library versions

--- test_install.py
+++ test_install.py
@@ -4,7 +4,7 @@ import torchvision
 import matplotlib
 import sklearn
 import numpy as np
-
+#test install
 print("✅ PennyLane version:", qml.__version__)
 print("✅ PyTorch version:", torch.__version__)
 print("✅ All libraries loaded successfully!")
